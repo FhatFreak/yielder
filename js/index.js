1ff51cb21616d91355fd49292ab91521d441e193
Joshua Belanger <jcbelanger@users.noreply.github.com>
2017-08-02 01:26:20 -0400

    buffer 1 finger drag to prevent drawing during 2 finger zoom

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 const svgns = 'http://www.w3.org/2000/svg';
 const mainSVG = document.getElementById('main');
 
-function toSvgPoint(svg, x, y) {
+const toSvgPoint = (svg, {x,y}) => {
     const p = svg.createSVGPoint();
     p.x = x;
     p.y = y;
     return p.matrixTransform(svg.getScreenCTM().inverse());
-}
+};
 
 const mouseDowns = Kefir.fromEvents(mainSVG, 'mousedown');
 const mouseMoves = Kefir.fromEvents(document, 'mousemove');
@@ -15,7 +15,7 @@ const mouseUps = Kefir.fromEvents(document, 'mouseup');
 const mouseDrags = mouseDowns.map(downEvent => mouseMoves
     .toProperty(_ => downEvent)
     .takeUntilBy(mouseUps)
-    .map(event => toSvgPoint(mainSVG, event.clientX, event.clientY)));
+    .map(( {clientX:x, clientY:y} ) => ( {x, y} )));
 
 const touchStarts = Kefir.fromEvents(mainSVG, 'touchstart');
 const touchMoves = Kefir.fromEvents(mainSVG, 'touchmove');
@@ -26,19 +26,29 @@ const singleTouchStarts = touchStarts.filter(event => event.touches.length == 1)
 const multiTouchStarts = touchStarts.filter(event => event.touches.length > 1);
 const touchDragStops = Kefir.merge([touchEnds, touchCancels, multiTouchStarts]);
 
-const touchDrags = singleTouchStarts
-    .onValue(event => event.preventDefault())
-    .map(startEvent => touchMoves
-        .toProperty(_ => startEvent)
+const touchDrags = singleTouchStarts.map(touchStart => {
+    const {touches:[ {clientX:x0, clientY:y0} ]} = touchStart;
+    const drag = touchMoves
         .takeUntilBy(touchDragStops)
-        .map(event => toSvgPoint(mainSVG, event.touches[0].clientX, event.touches[0].clientY)));
+        .toProperty(_ => touchStart)
+        .onValue(event => event.preventDefault())
+        .map(( {touches:[ {clientX:x, clientY:y} ]} ) => ( {x, y} ));
+
+    const radius = 10;
+    const outsideRadius = drag
+        .filter(( {x, y} ) => Math.sqrt((x - x0)**2 + (y - y0)**2) >= radius)
+        .take(1);
+
+    return drag
+        .bufferBy(outsideRadius, {flushOnEnd:false})
+        .flatten()
+        .concat(drag.skipUntilBy(outsideRadius));
+});
 
 const drags = Kefir.merge([touchDrags, mouseDrags])
-    .map(drag => drag.skipDuplicates((a, b) => {
-        const {x:x1, y:y1} = a;
-        const {x:x2, y:y2} = b;
-        return x1 === x2 && y1 === y2;
-    }));
+    .map(drag => drag
+        .skipDuplicates(( {x:x1, y:y1}, {x:x2, y:y2} ) => x1 === x2 && y1 === y2)
+        .map( point => toSvgPoint(mainSVG, point)));
 
 drags.onValue(drag => drag
     .slidingWindow(2, 2)
