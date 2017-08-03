7ed716aec73dbd3878bfbc4b1cdd2f1fa8264974
Joshua Belanger <jcbelanger@users.noreply.github.com>
2017-08-03 14:09:53 -0400

    trace linear regression line as you draw

--- js/index.js
+++ js/index.js
@@ -44,18 +44,89 @@ const touchDrawPaths = singleTouchStarts
         .map(( {touches:[ {clientX:x, clientY:y} ]} ) => ( {x, y} )))
     .map(pathRadiusBuffer);
 
-const drawPaths = Kefir.merge([touchDrawPaths, mouseDrawPaths]).map(path => path
-    .skipDuplicates(( {x:x1, y:y1}, {x:x2, y:y2} ) => x1 === x2 && y1 === y2)
-    .map(point => toSvgPoint(mainSVG, point)));
+const drawPaths = Kefir.merge([touchDrawPaths, mouseDrawPaths])
+    .map(path => path
+        .skipDuplicates(( {x:x1, y:y1}, {x:x2, y:y2} ) => x1 === x2 && y1 === y2)
+        .map(point => toSvgPoint(mainSVG, point)));
 
-drawPaths.onValue(path => path
-    .slidingWindow(2, 2)
-    .onValue(( [{x:x1, y:y1}, {x:x2, y:y2}] ) => {
-        const line = document.createElementNS(svgns, 'line');
-        line.setAttribute('x1', x1);
-        line.setAttribute('y1', y1);
-        line.setAttribute('x2', x2);
-        line.setAttribute('y2', y2);
-        line.setAttribute('stroke', 'black');
-        mainSVG.appendChild(line);
-    }));
+const linearLeastSquares = (path) => {
+    //see: https://en.wikipedia.org/wiki/Linear_least_squares_(mathematics)
+    const S = path.scan(( {b1_2, b2_2, b1b2, b1, b2, c}, {x, y} ) => {
+        return {
+            b1_2: b1_2 + 1,
+            b2_2: b2_2 + x*x,
+            b1b2: b1b2 + 2*x,
+            b1: b1 + -2*y,
+            b2: b2 + -2*x*y,
+            c: c + y*y
+        };
+    }, {b1_2:0, b2_2:0, b1b2:0, b1:0, b2:0, c:0});
+
+    const dS_db1 = S.map(( {b1_2, b2_2, b1b2, b1, b2, c} ) => {
+        return {
+            b1: 2*b1_2,
+            b2: b1b2,
+            c: b1
+        };
+    });
+
+    const dS_db2 = S.map(( {b1_2, b2_2, b1b2, b1, b2, c} ) => {
+        return {
+            b1: b1b2,
+            b2: 2*b2_2,
+            c: b2
+        };
+    });
+
+    return Kefir.zip([dS_db1, dS_db2], ( {b1:a, b2:b, c:c}, {b1:x, b2:y, c:z} ) => {
+        //TODO: check for no solutions/infinite solutions/vetical lines
+        const eq1 = [a,b,-c];
+        const eq2 = [x,y,-z];
+        const [i,j,k] = eq1.map((num, ix) => -x/a * num + eq2[ix]);
+        const b2 = k/j;
+        const b1 = -(c+b*b2)/a;
+        return {b2, b1};
+    });
+};
+
+//drawPaths
+const test = Kefir.sequentially(1, [
+    {x:1, y:6},
+    {x:2, y:5},
+    {x:3, y:7},
+    {x:4, y:10}
+]);
+
+drawPaths
+    .map(linearLeastSquares)
+    .onValue(fits => fits
+        .filter(( {b2, b1} ) => !isNaN(b2) && !isNaN(b1))
+        .map(( {b2, b1} ) => {
+            const x = 200;
+            const y = x*b2 + b1
+            const line = document.createElementNS(svgns, 'line');
+            line.setAttribute('x1', 0);
+            line.setAttribute('y1', b1);
+            line.setAttribute('x2', x);
+            line.setAttribute('y2', y);
+            line.setAttribute('stroke', 'red');
+            return line;
+        })
+        .slidingWindow(2, 2)
+        .onValue(( [prev, next] ) => {
+            prev.remove();
+            mainSVG.appendChild(next);
+        }));
+
+drawPaths
+    .onValue(path => path
+        .slidingWindow(2, 2)
+        .onValue(( [{x:x1, y:y1}, {x:x2, y:y2}] ) => {
+            const line = document.createElementNS(svgns, 'line');
+            line.setAttribute('x1', x1);
+            line.setAttribute('y1', y1);
+            line.setAttribute('x2', x2);
+            line.setAttribute('y2', y2);
+            line.setAttribute('stroke', 'black');
+            mainSVG.appendChild(line);
+        }));
